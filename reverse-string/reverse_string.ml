208eb44cbe5778cfb12fd249a78c2ce95613e637
ktunprasert <ktunprasert@outlook.com>
2025-06-03 14:53:45 +0100

    rev-string: wow

--- reverse-string/reverse_string.ml
+++ reverse-string/reverse_string.ml
@@ -1,10 +1,3 @@
-let reverse_string str =
-  let rec rev str acc =
-    match str with
-    | "" -> acc
-    | _ ->
-        rev
-          (String.sub str 1 (String.length str - 1))
-          (String.make 1 (String.get str 0) ^ acc)
-  in
-  rev str ""
+open Base
+
+let reverse_string = String.rev
