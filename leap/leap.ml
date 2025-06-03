64a1d20666a8ead3ae6b871d40cd6803feeeef1c
ktunprasert <ktunprasert@outlook.com>
2025-06-03 13:58:32 +0100

    leap: func guard

--- leap/leap.ml
+++ leap/leap.ml
@@ -1,6 +1,4 @@
-let leap_year (n : int) : bool =
-  match (n mod 400, n mod 100, n mod 4) with
-  | 0, _, _ -> true
-  | _, 0, _ -> false
-  | _, _, 0 -> true
-  | _ -> false
+let leap_year = function
+  | n when n mod 400 = 0 -> true
+  | n when n mod 100 = 0 -> false
+  | n -> n mod 4 == 0
