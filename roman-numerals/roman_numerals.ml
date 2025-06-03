a07e06961df5d1e852e69b46e4c1412bd6d36469
ktunprasert <ktunprasert@outlook.com>
2025-06-03 16:03:41 +0100

    roman-num: tail-call

--- roman-numerals/roman_numerals.ml
+++ roman-numerals/roman_numerals.ml
@@ -1,16 +1,19 @@
-let rec to_roman (n : int) : string =
-  match n with
-  | n when n >= 1000 -> "M" ^ to_roman (n - 1000)
-  | n when n >= 900 -> "CM" ^ to_roman (n - 900)
-  | n when n >= 500 -> "D" ^ to_roman (n - 500)
-  | n when n >= 400 -> "CD" ^ to_roman (n - 400)
-  | n when n >= 100 -> "C" ^ to_roman (n - 100)
-  | n when n >= 90 -> "XC" ^ to_roman (n - 90)
-  | n when n >= 50 -> "L" ^ to_roman (n - 50)
-  | n when n >= 40 -> "XL" ^ to_roman (n - 40)
-  | n when n >= 10 -> "X" ^ to_roman (n - 10)
-  | n when n >= 9 -> "IX" ^ to_roman (n - 9)
-  | n when n >= 5 -> "V" ^ to_roman (n - 5)
-  | n when n >= 4 -> "IV" ^ to_roman (n - 4)
-  | n when n >= 1 -> "I" ^ to_roman (n - 1)
-  | _ -> ""
+let to_roman (n : int) : string =
+  let rec aux current_n acc =
+    match current_n with
+    | n when n >= 1000 -> aux (n - 1000) ("M" :: acc)
+    | n when n >= 900 -> aux (n - 900) ("CM" :: acc)
+    | n when n >= 500 -> aux (n - 500) ("D" :: acc)
+    | n when n >= 400 -> aux (n - 400) ("CD" :: acc)
+    | n when n >= 100 -> aux (n - 100) ("C" :: acc)
+    | n when n >= 90 -> aux (n - 90) ("XC" :: acc)
+    | n when n >= 50 -> aux (n - 50) ("L" :: acc)
+    | n when n >= 40 -> aux (n - 40) ("XL" :: acc)
+    | n when n >= 10 -> aux (n - 10) ("X" :: acc)
+    | n when n >= 9 -> aux (n - 9) ("IX" :: acc)
+    | n when n >= 5 -> aux (n - 5) ("V" :: acc)
+    | n when n >= 4 -> aux (n - 4) ("IV" :: acc)
+    | n when n >= 1 -> aux (n - 1) ("I" :: acc)
+    | _ -> String.concat "" (List.rev acc)
+  in
+  aux n []
